912e373712829418b0b0c2448e9eaa338f0bcdbd
Jhonatan Muñoz <Jhonm21@gmail.com>
2025-09-06 15:10:44 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -103,7 +103,7 @@ export function Navbar() {
             <NavigationMenuList>
               {navLinks.map((link) => (
                 <NavigationMenuItem key={link.href}>
-                  <Link href={link.href} passHref>
+                  <Link href={link.href} legacyBehavior passHref>
                     <NavigationMenuLink
                       className={cn(
                         navigationMenuTriggerStyle(),
@@ -235,7 +235,7 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
@@ -247,7 +247,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   );
